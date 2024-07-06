19bc0685e7924df07734432b717441c744390b3a
Kajdsky1337 <mateusz_kukuczka@techniplast.pl>
2024-07-06 09:37:24 +0200

    Add files via upload

---
+++ DataAccess/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskMaster.DataAccess.Models;
+
+namespace TaskMaster.DataAccess
+{
+    public class Repository
+    {
+        List<Employee> Employees { get; set; }
+        List<TaskForEmployee> TasksForEmployees { get; set; }
+       
+        public List<Employee> GetEmployees()
+        {
+
+            return Employees;
+
+        }
+        public List<TaskForEmployee> GetTasks()
+        {
+            return TasksForEmployees;
+        }
+        public void AddEmployee(Employee employee)
+        {
+            Employees.Add(employee);
+        }
+        public void AddTask(TaskForEmployee task)
+        {
+            TasksForEmployees.Add(task);
+        }
+    }
+}
